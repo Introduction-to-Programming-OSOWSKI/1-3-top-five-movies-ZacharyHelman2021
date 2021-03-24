f6cae8feee91cef3a2555f8728813646cf5c7029
ZacharyHelman2021 <828664@mystma.org>
2021-03-24 17:16:38 +0000

    TopFiveMovies try 4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(a, b, c, d, e):
+def topFiveMovies(a,b,c,d,e):
     return a+b+c+d+e
 
 print(topFiveMovies("Raging Bull ", "Once Upon a Time in the West ", "Dunkirk ", "Bladerunner ", "Vertigo"))
